e774afa695e36c0ca6f450ea7d8d9935c7b832c9
albae69 <ahmadalbaihaqi69@gmail.com>
2020-08-12 21:02:52 +0700

    initial commit

---
+++ precache-manifest.60374662a76e37b7e689f5671eeabe77.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed726f6b02f629708e64e1e31b67588a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34975146cf696a29854e",
+    "url": "/static/css/main.50950e3a.chunk.css"
+  },
+  {
+    "revision": "0e9891e1dc990dd05044",
+    "url": "/static/js/2.c5e99a1e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c5e99a1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34975146cf696a29854e",
+    "url": "/static/js/main.bcb685f7.chunk.js"
+  },
+  {
+    "revision": "144613ff0e45ab54d31c",
+    "url": "/static/js/runtime-main.13eae8a7.js"
+  },
+  {
+    "revision": "8cbb732fce4dc52f8e9a80335e35fc83",
+    "url": "/static/media/bae-pondok.8cbb732f.jpg"
+  },
+  {
+    "revision": "3287b2eb91b25f9b8c8cad64431c8e4f",
+    "url": "/static/media/fb.3287b2eb.svg"
+  },
+  {
+    "revision": "a1bd1c96886f731aff13a7f6bb204e4c",
+    "url": "/static/media/github.a1bd1c96.svg"
+  },
+  {
+    "revision": "f6e096d0b6ba120528e4c17e88c86efe",
+    "url": "/static/media/ig.f6e096d0.svg"
+  }
+]);
