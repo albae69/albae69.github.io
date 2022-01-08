1e3bcadacb2cdc846c5797148d4c08424122f52e
albae69 <ahmadalbaihaqi69@gmail.com>
2022-01-09 04:39:51 +0700

    update

---
+++ precache-manifest.97501f785a25b31e25b02377a7565ae3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e4b2a077c226ce4c139820dd5340fdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4acc0691cebf728bc293",
+    "url": "/static/css/main.82ba4da0.chunk.css"
+  },
+  {
+    "revision": "fa113ed9e54b44ea03cf",
+    "url": "/static/js/2.3716d6e7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.3716d6e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4acc0691cebf728bc293",
+    "url": "/static/js/main.01b4a657.chunk.js"
+  },
+  {
+    "revision": "01e0670ec1b45bd51b9f",
+    "url": "/static/js/runtime-main.7ac829d2.js"
+  },
+  {
+    "revision": "3287b2eb91b25f9b8c8cad64431c8e4f",
+    "url": "/static/media/fb.3287b2eb.svg"
+  },
+  {
+    "revision": "a1bd1c96886f731aff13a7f6bb204e4c",
+    "url": "/static/media/github.a1bd1c96.svg"
+  },
+  {
+    "revision": "f6e096d0b6ba120528e4c17e88c86efe",
+    "url": "/static/media/ig.f6e096d0.svg"
+  },
+  {
+    "revision": "5b6b0f5893db135cd00d62ff51427f2b",
+    "url": "/static/media/img.5b6b0f58.png"
+  },
+  {
+    "revision": "69eb0e5b6468bb234f7c1c1bb1acf73b",
+    "url": "/static/media/img_1.69eb0e5b.png"
+  },
+  {
+    "revision": "2d27cc5135da351641b5bc2002640381",
+    "url": "/static/media/linkedin.2d27cc51.svg"
+  }
+]);
