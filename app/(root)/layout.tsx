ed6e30eab74bc4975cd3cc95f1ef9a8596a6d9f3
albae69 <ahmadalbaihaqi69@gmail.com>
2023-09-21 16:17:59 +0700

    added: analytics

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { twMerge } from 'tailwind-merge'
@@ -33,6 +34,7 @@ export default function RootLayout({
           {children}
           <Footer />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
