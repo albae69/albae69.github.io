393a8f52131fe6539638b3b34979018f96508206
albae69 <ahmadalbaihaqi69@gmail.com>
2026-09-25 19:39:16 +0700

    🚀

---
+++ assets/hello_word-BoBMnMKL.js
@@ -0,0 +1,4 @@
+import{j as t}from"./main-D-D9RmEd.js";import"./index-BiohcOg1.js";function s(n){const e={p:"p",...n.components};return t.jsxs(t.Fragment,{children:[t.jsxs(e.p,{children:[t.jsx("h1",{className:"text-black font-bold text-2xl",children:"Hello World!"}),`\r
+12 June 2025`]}),`
+`,t.jsx("br",{}),`
+`,t.jsx(e.p,{children:"This is my first post build with mdx-js/react."})]})}function c(n={}){const{wrapper:e}=n.components||{};return e?t.jsx(e,{...n,children:t.jsx(s,{...n})}):s(n)}export{c as default};
